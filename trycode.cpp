d8d7d63812b0f9beff931fa1255d5fc2bc764008
Krish patel <krishhpatel1905@gmail.com>
2025-05-26 20:40:52 +0530

    Final Commit

--- trycode.cpp
+++ trycode.cpp
@@ -1,19 +1,7 @@
-#include <fstream>
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-     fstream file;
-    file.open("data.txt", ios::app); // Write mode
-    file << "krish hitesh bhai" << endl;
-    file.close();
-
-    file.open("data.txt", ios::in); // Read mode
-    string text;
-    while (getline(file, text)) {
-        cout << text << endl;
-    }
-    file.close();
+    cout << "Hello, World!" << endl;
     return 0;
 }
