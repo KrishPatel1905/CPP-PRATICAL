3b7615818f1a06169ab0b414be870e59689100a3
Krish patel <krishhpatel1905@gmail.com>
2025-05-27 19:15:39 +0530

    Final Commit

--- trycode.cpp
+++ trycode.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    srand(time(0));  // Seed for random number generation
+    int secretNumber = rand() % 100 + 1; // Random number between 1 and 100
+    int guess;
+    int attempts = 0;
+
+    cout << "🎮 Welcome to the Number Guessing Game!" << endl;
+    cout << "Guess the number (between 1 and 100): ";
+
+    do {
+        cin >> guess;
+        attempts++;
+
+        if (guess > secretNumber) {
+            cout << "Too high! Try again: ";
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again: ";
+        } else {
+            cout << "🎉 Congratulations! You guessed it in " << attempts << " attempts." << endl;
+        }
+    } while (guess != secretNumber);
+
     return 0;
 }
